e78c26431f69ca69f2d71d3b4e1067ee6722132f
Cameron Tullos <ctullos@resourceammirati.com>
2018-01-18 08:37:14 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+function createThunkMiddleware(extraArgument) {
+    return (store) => next => action => {
+        if (typeof action === 'function') {
+            return action(store.dispatch, store.getState, store, extraArgument);
+        }
+
+        return next(action);
+    };
+}
+
+const thunk = createThunkMiddleware();
+thunk.withExtraArgument = createThunkMiddleware;
+
+export default thunk;
